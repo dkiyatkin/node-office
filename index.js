c26aca7b0a95a89c235d01a55d1617151b99347c
Dmitriy Kiyatkin <d.kiyatkin@itlf.ru>
2012-08-05 14:32:29 +0400

    parse spreadsheets

---
+++ index.js
@@ -0,0 +1,76 @@
+var fs = require('fs');
+var path = require('path');
+var exec = require('child_process').exec;
+var xmlParser = require('xml2json');
+var temp = require('temp');
+
+var office = {
+	spreadsheets: ['xls', 'xlsx', 'ods'],
+	documents: ['doc', 'docx', 'odt'],
+	xlsParse: function(file, callback) {
+		exec('xlhtml -xml ' + file, function(error, stdout, stderr) {
+			if(stderr) { console.error(stderr) };
+			if (!error) {
+				callback(null, xmlParser.toJson(stdout, {object: true}).excel_workbook);
+			} else { callback(error); }
+		});
+	},
+	spreadsheetParse: function(filename, options, cb) {
+		var self = this;
+		if (options.ext != 'xls') {
+			var tempname = temp.path({suffix: '.xls'});
+			exec('unoconv --stdout --format=xls ' + filename + ' > ' + tempname, function(error, stdout, stderr) {
+				if(stderr) { console.error(stderr) };
+				if (!error) {
+					self.xlsParse(tempname, function(err, data) {
+						fs.unlink(tempname, function(e) {
+							if (e) { console.error(e); }
+							cb(err, data);
+						});
+					});
+				} else { cb(error); }
+			});
+		} else { self.xlsParse(filename, cb); }
+	},
+	getType: function(filename, cb) {
+		// проверка по расширению
+		var self = this;
+		fs.exists(filename, function(exists) {
+			if (!exists) {
+				cb(new Error('file does not exist'));
+			} else {
+				var ext = path.extname(filename).toLowerCase().slice(1);
+				if (self.spreadsheets.indexOf(ext) != -1) {
+					cb(null, 'spreadsheet', ext);
+				} else if (self.documents.indexOf(ext) != -1) {
+					cb(null, 'document');
+				} else {
+					// TODO проверка по mime-type
+					cb(new Error('wrong file format'));
+				}
+			}
+		});
+	},
+	parse: function(filename, options, cb) {
+		var self = this;
+		if (!cb && typeof(options) == 'function') { cb = options; options = { } }
+		self.getType(filename, function(err, type, ext) {
+			options.ext = ext;
+			if (!err) {
+				if (type == 'spreadsheet') {
+					self.spreadsheetParse(filename, options, function(err, data) {
+						cb(err, data);
+					});
+				} else if (type == 'document') {
+					self.documentParse(filename, options, function(err, data) {
+						cb(err, data);
+					});
+				} else {
+					cb(new Error('wrong file format'));
+				}
+			} else { cb(err); }
+		});
+	}
+};
+
+module.exports = office;
---
+++ test/spreadsheets.js
@@ -0,0 +1,37 @@
+/* Тест обработки электронных таблиц */
+
+var office = require('../index.js');
+
+/* xls */
+exports.xls = function(test) {
+	test.expect(5);
+	office.parse(__dirname + '/test.xls', function(err, data) {
+		test.ok(!err, err);
+		var h = data.sheets.sheet[0].rows.row[0].cell[0]['$t'];
+		var t = data.sheets.sheet[0].rows.row[0].cell[1]['$t'];
+		var a = data.sheets.sheet[0].rows.row[1].cell[0]['$t'];
+		var b = data.sheets.sheet[0].rows.row[1].cell[1]['$t'];
+		test.strictEqual(h, 'hello', 'json data');
+		test.strictEqual(t, 'test', 'json data');
+		test.strictEqual(a, '1', 'json data');
+		test.strictEqual(b, '2', 'json data');
+		test.done();
+	});
+};
+
+/* ods */
+exports.ods = function(test) {
+	test.expect(5);
+	office.parse(__dirname + '/test.ods', function(err, data) {
+		test.ok(!err, err);
+		var h = data.sheets.sheet[0].rows.row[0].cell[0]['$t'];
+		var t = data.sheets.sheet[0].rows.row[0].cell[1]['$t'];
+		var a = data.sheets.sheet[0].rows.row[1].cell[0]['$t'];
+		var b = data.sheets.sheet[0].rows.row[1].cell[1]['$t'];
+		test.strictEqual(h, 'hello', 'json data');
+		test.strictEqual(t, 'test', 'json data');
+		test.strictEqual(a, '1', 'json data');
+		test.strictEqual(b, '2', 'json data');
+		test.done();
+	});
+};
